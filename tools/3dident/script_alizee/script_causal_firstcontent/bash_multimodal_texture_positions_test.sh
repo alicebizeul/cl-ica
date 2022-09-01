1c30f3f55484c8ec66b6b0716b0499e82fa4199f
Alice Elsa Marie Bizeul <alice.bizeul@inf.ethz.ch>
2022-09-01 21:32:57 +0200

    scipts for alizee account

--- tools/3dident/script_alizee/script_causal_firstcontent/bash_multimodal_texture_positions_test.sh
+++ tools/3dident/script_alizee/script_causal_firstcontent/bash_multimodal_texture_positions_test.sh
@@ -1,23 +1,25 @@
 #!/bin/bash 
-#SBATCH -o /cluster/work/vogtlab/Group/abizeul/multimodal_texture_positions_test_causal.out
+#SBATCH -o /cluster/work/grlab/projects/projects2022-identifiability/multimodal_texture_positions_test_causal.out
 #SBATCH --time=4:00:00
 #SBATCH -p gpu
 #SBATCH --gres=gpu:rtx2080ti:1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem-per-cpu=10G
+#SBATCH -A grlab
+#SBATCH --priority=167661
 
 DIR_EXPERIMENT="$PWD/runs/tmp"  # NOTE: experiment logs are written here
 
 
 ############# PLEASE CHANGE THE FOLLOWING PARAMETERS ###############
-LD_LIBRARY_PATH="/cluster/home/abizeul/software/anaconda/envs/kugelen/lib/:$LD_LIBRARY_PATH"
-CONDA_PATH=/cluster/home/abizeul/software/anaconda/etc/profile.d/conda.sh 
-SCRIPT_DIR=/cluster/home/abizeul/cl-ica/tools/3dident
-BLENDER_DIR=/cluster/home/abizeul/software/blender-2.90.1-linux64/blender
+LD_LIBRARY_PATH="/cluster/customapps/biomed/grlab/users/alpace/miniconda3/envs/offlineRL/lib/:$LD_LIBRARY_PATH"
+CONDA_PATH=/cluster/customapps/biomed/grlab/users/alpace/miniconda3/etc/profile.d/conda.sh 
+SCRIPT_DIR=/cluster/work/grlab/projects/projects2022-identifiability/cl-ica/tools/3dident
+BLENDER_DIR=/cluster/work/grlab/projects/projects2022-identifiability/blender-2.90.1-linux64/blender
 DATA_NAME="mydata_positions_test_causal_firstcontent"  # include mention to test/val/train, MS and C/S settings
-DIR_DATA="/cluster/work/vogtlab/Group/abizeul/$DATA_NAME"
-DIR_ZIP="/cluster/work/vogtlab/Group/abizeul/3DIdent/"
-N_POINTS=10000  # if test/validation fix this to 10 000, for train fix this to 250 000
+DIR_DATA="/cluster/work/grlab/projects/projects2022-identifiability/$DATA_NAME"
+DIR_ZIP="/cluster/work/grlab/projects/projects2022-identifiability/"
+N_POINTS=10000 # if test/validation fix this to 10 000, for train fix this to 250 000
 #####################################################################
 
 N_BATCHES=10
