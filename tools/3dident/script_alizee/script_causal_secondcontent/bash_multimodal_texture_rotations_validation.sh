5614874d8a87961196ab69513b52e0fa5e3098c9
Alice Elsa Marie Bizeul <alice.bizeul@inf.ethz.ch>
2022-09-02 00:43:35 +0200

    update scripts alizee

--- tools/3dident/script_alizee/script_causal_secondcontent/bash_multimodal_texture_rotations_validation.sh
+++ tools/3dident/script_alizee/script_causal_secondcontent/bash_multimodal_texture_rotations_validation.sh
@@ -1,33 +1,48 @@
 #!/bin/bash 
-#SBATCH -o /cluster/work/vogtlab/Group/abizeul/multimodal_texture_rotations_val_causal.out
-#SBATCH --time=60:00:00
+#SBATCH -o /cluster/work/grlab/projects/projects2022-identifiability/multimodal_texture_rotations_val_causal.out
+#SBATCH --time=4:00:00
 #SBATCH -p gpu
 #SBATCH --gres=gpu:rtx2080ti:1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem-per-cpu=10G
+#SBATCH -A grlab
+#SBATCH --priority=167668
 
 DIR_EXPERIMENT="$PWD/runs/tmp"  # NOTE: experiment logs are written here
-LD_LIBRARY_PATH="/cluster/home/abizeul/software/anaconda/envs/kugelen/lib/:$LD_LIBRARY_PATH"
 
-source /cluster/home/abizeul/software/anaconda/etc/profile.d/conda.sh 
-conda activate kugelen
 
-cd /cluster/home/abizeul/cl-ica/tools/3dident
-DIR_DATA="/cluster/work/vogtlab/Group/abizeul/mydata_rotations_val_causal"
+############# PLEASE CHANGE THE FOLLOWING PARAMETERS ###############
+LD_LIBRARY_PATH="/cluster/customapps/biomed/grlab/users/alpace/miniconda3/envs/offlineRL/lib/:$LD_LIBRARY_PATH"
+CONDA_PATH=/cluster/customapps/biomed/grlab/users/alpace/miniconda3/etc/profile.d/conda.sh 
+SCRIPT_DIR=/cluster/work/grlab/projects/projects2022-identifiability/cl-ica/tools/3dident
+BLENDER_DIR=/cluster/work/grlab/projects/projects2022-identifiability/blender-2.90.1-linux64/blender
+DATA_NAME="mydata_rotations_val_causal"  # include mention to test/val/train, MS and C/S settings
+DIR_DATA="/cluster/work/grlab/projects/projects2022-identifiability/$DATA_NAME"
+DIR_ZIP="/cluster/work/grlab/projects/projects2022-identifiability/"
+N_POINTS=10000 # if test/validation fix this to 10 000, for train fix this to 250 000
+ENV=offlineRL
+#####################################################################
 
-python generate_clevr_dataset_latents_causal.py --output-folder ${DIR_DATA} --n-points 10000 --non-periodic-rotation-and-color --deterministic --all-rotations --multimodal
-/cluster/home/abizeul/software/blender-2.90.1-linux64/blender -noaudio --background --python generate_clevr_dataset_images.py --use-gpu --output-folder ${DIR_DATA} --n-batches 10 --batch-index 0 --material-names "MyMetal" --no_range_change
-/cluster/home/abizeul/software/blender-2.90.1-linux64/blender -noaudio --background --python generate_clevr_dataset_images.py --use-gpu --output-folder ${DIR_DATA} --n-batches 10 --batch-index 1 --material-names "MyMetal" --no_range_change
-/cluster/home/abizeul/software/blender-2.90.1-linux64/blender -noaudio --background --python generate_clevr_dataset_images.py --use-gpu --output-folder ${DIR_DATA} --n-batches 10 --batch-index 2 --material-names "MyMetal" --no_range_change
-/cluster/home/abizeul/software/blender-2.90.1-linux64/blender -noaudio --background --python generate_clevr_dataset_images.py --use-gpu --output-folder ${DIR_DATA} --n-batches 10 --batch-index 3 --material-names "MyMetal" --no_range_change
-/cluster/home/abizeul/software/blender-2.90.1-linux64/blender -noaudio --background --python generate_clevr_dataset_images.py --use-gpu --output-folder ${DIR_DATA} --n-batches 10 --batch-index 4 --material-names "MyMetal" --no_range_change
-/cluster/home/abizeul/software/blender-2.90.1-linux64/blender -noaudio --background --python generate_clevr_dataset_images.py --use-gpu --output-folder ${DIR_DATA} --n-batches 10 --batch-index 5 --material-names "Rubber" --no_range_change
-/cluster/home/abizeul/software/blender-2.90.1-linux64/blender -noaudio --background --python generate_clevr_dataset_images.py --use-gpu --output-folder ${DIR_DATA} --n-batches 10 --batch-index 6 --material-names "Rubber" --no_range_change
-/cluster/home/abizeul/software/blender-2.90.1-linux64/blender -noaudio --background --python generate_clevr_dataset_images.py --use-gpu --output-folder ${DIR_DATA} --n-batches 10 --batch-index 7 --material-names "Rubber" --no_range_change
-/cluster/home/abizeul/software/blender-2.90.1-linux64/blender -noaudio --background --python generate_clevr_dataset_images.py --use-gpu --output-folder ${DIR_DATA} --n-batches 10 --batch-index 8 --material-names "Rubber" --no_range_change
-/cluster/home/abizeul/software/blender-2.90.1-linux64/blender -noaudio --background --python generate_clevr_dataset_images.py --use-gpu --output-folder ${DIR_DATA} --n-batches 10 --batch-index 9 --material-names "Rubber" --no_range_change
+N_BATCHES=10
+mkdir -p ${DIR_DATA}
 
+source ${CONDA_PATH}
+conda activate ${ENV}
+
+cd ${SCRIPT_DIR}
+
+python generate_clevr_dataset_latents_causal.py --output-folder ${DIR_DATA} --n-points ${N_POINTS} --non-periodic-rotation-and-color --deterministic --all-rotations --multimodal
+for (( i=0; i<=$N_BATCHES; i++ ))
+do
+    MATERIAL="MyMetal"
+    if [[ $i -ge 5 ]] 
+    then
+        MATERIAL="Rubber"
+    fi
+    echo $MATERIAL
+
+    ${BLENDER_DIR} -noaudio --background --python generate_clevr_dataset_images.py --use-gpu --output-folder ${DIR_DATA} --n-batches 10 --batch-index ${i} --material-names ${MATERIAL} --no_range_change
+done
 
 zip -r "$DIR_DATA.zip" ${DIR_DATA}
-scp "$DIR_DATA.zip" "/cluster/work/vogtlab/Group/abizeul/3DIdent/mydata_multimodal_texture_rotations_val_causal.zip"
-
+scp "$DIR_DATA.zip" "${DIR_ZIP}/${DATA_NAME}.zip"
