3f5b0a0ec2db0adc2f53ea30f2b6ea9a0741714c
Alice Elsa Marie Bizeul <alice.bizeul@inf.ethz.ch>
2022-09-03 14:14:27 +0200

    adding positions dataset

--- tools/3dident/script_alessandro/script_causal_firstcontent/bash_multimodal_texture_positions_train.sh
+++ tools/3dident/script_alessandro/script_causal_firstcontent/bash_multimodal_texture_positions_train.sh
@@ -11,7 +11,7 @@ DIR_EXPERIMENT="$PWD/runs/tmp"  # NOTE: experiment logs are written here
 CONDA_PATH="/cluster/project/sachan/alessandro/miniconda3/etc/profile.d/conda.sh"
 SCRIPT_DIR=/cluster/home/stolfoa/alice/cl-ica/tools/3dident
 BLENDER_DIR=/cluster/scratch/stolfoa/alice_experiments/blender-2.90.1-linux64/blender
-DATA_NAME="mydata_positions_firstcontent_train"  # include mention to test/val/train, MS and C/S settings
+DATA_NAME="mydata_multimodal_texture_positions_firstcontent/3dident/train"  # include mention to test/val/train, MS and C/S settings
 DIR_DATA="/cluster/scratch/stolfoa/alice_dataset/$DATA_NAME"
 N_POINTS=250000  # if test/validation fix this to 10 000, for train fix this to 250 000
 #####################################################################
