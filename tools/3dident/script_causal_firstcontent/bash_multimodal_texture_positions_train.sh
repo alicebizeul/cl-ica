8043de290956150b2cdd963ed6ba8e2db80ff009
Alice Elsa Marie Bizeul <alice.bizeul@inf.ethz.ch>
2022-09-22 10:18:32 +0200

    remaining of causal models

--- tools/3dident/script_causal_firstcontent/bash_multimodal_texture_positions_train.sh
+++ tools/3dident/script_causal_firstcontent/bash_multimodal_texture_positions_train.sh
@@ -1,8 +1,8 @@
 #!/bin/bash 
 #SBATCH -o /cluster/work/vogtlab/Group/abizeul/multimodal_texture_positions_train_causal.out
-#SBATCH --time=60:00:00
+#SBATCH --time=48:00:00
 #SBATCH -p gpu
-#SBATCH --gres=gpu:rtx2080ti:1
+#SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem-per-cpu=10G
 
@@ -14,22 +14,23 @@ LD_LIBRARY_PATH="/cluster/home/abizeul/software/anaconda/envs/kugelen/lib/:$LD_L
 CONDA_PATH=/cluster/home/abizeul/software/anaconda/etc/profile.d/conda.sh 
 SCRIPT_DIR=/cluster/home/abizeul/cl-ica/tools/3dident
 BLENDER_DIR=/cluster/home/abizeul/software/blender-2.90.1-linux64/blender
-DATA_NAME="mydata_positions_train_causal_firstcontent"  # include mention to test/val/train, MS and C/S settings
+DATA_NAME="mydata_positions_causal_firstcontent/3dident/train"  # include mention to test/val/train, MS and C/S settings
 DIR_DATA="/cluster/work/vogtlab/Group/abizeul/$DATA_NAME"
-DIR_ZIP="/cluster/work/vogtlab/Group/abizeul/3DIdent/"
-N_POINTS=250000  # if test/validation fix this to 10 000, for train fix this to 250 000
+DIR_ZIP="/cluster/work/vogtlab/Group/abizeul/"
+N_POINTS=250000 # if test/validation fix this to 10 000, for train fix this to 250 000
+ENV=kugelen
 #####################################################################
 
 N_BATCHES=10
-mkdir -p ${DIR_DATA}
+#mkdir -p ${DIR_DATA}
 
 source ${CONDA_PATH}
-conda activate kugelen
+conda activate ${ENV}
 
 cd ${SCRIPT_DIR}
 
-python generate_clevr_dataset_latents_causal.py --output-folder ${DIR_DATA} --n-points ${N_POINTS} --non-periodic-rotation-and-color --deterministic --all-positions --multimodal --first_content
-for (( i=0; i<=$N_BATCHES; i++ ))
+#python generate_clevr_dataset_latents_causal.py --output-folder ${DIR_DATA} --n-points ${N_POINTS} --non-periodic-rotation-and-color --deterministic --all-positions --multimodal --first_content
+for (( i=9; i<$N_BATCHES; i++ ))
 do
     MATERIAL="MyMetal"
     if [[ $i -ge 5 ]] 
