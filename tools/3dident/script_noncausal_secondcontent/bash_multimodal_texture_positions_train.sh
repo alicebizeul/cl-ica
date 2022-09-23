b48c5600efae31ff0b9173b52b79a9e39264bf93
Alice Elsa Marie Bizeul <alice.bizeul@inf.ethz.ch>
2022-09-23 22:50:10 +0200

    ading position script

--- tools/3dident/script_noncausal_secondcontent/bash_multimodal_texture_positions_train.sh
+++ tools/3dident/script_noncausal_secondcontent/bash_multimodal_texture_positions_train.sh
@@ -1,32 +1,37 @@
 #!/bin/bash 
-#SBATCH -o /cluster/work/vogtlab/Group/abizeul/multimodal_texture_positions_train.out
-#SBATCH --time=60:00:00
+#SBATCH -oo /cluster/work/vogtlab/Group/abizeul/multimodal_texture_positions_train.out
 #SBATCH -p gpu
-#SBATCH --gres=gpu:rtx2080ti:1
+#SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem-per-cpu=10G
 
 DIR_EXPERIMENT="$PWD/runs/tmp"  # NOTE: experiment logs are written here
-LD_LIBRARY_PATH="/cluster/home/abizeul/software/anaconda/envs/kugelen/lib/:$LD_LIBRARY_PATH"
 
-source /cluster/home/abizeul/software/anaconda/etc/profile.d/conda.sh 
-conda activate kugelen
+############# PLEASE CHANGE THE FOLLOWING PARAMETERS ###############
+CONDA_PATH="/cluster/home/abizeul/software/anaconda/etc/profile.d/conda.sh"
+SCRIPT_DIR=/cluster/home/abizeul/alice-clica/cl-ica/tools/3dident
+BLENDER_DIR=/cluster/home/abizeul/software/blender-2.90.1-linux64/blender
+DATA_NAME="mydata_multimodal_texture_positions/3dident/train"  # include mention to test/val/train, MS and C/S settings
+DIR_DATA="/cluster/home/abizeul/$DATA_NAME"
+N_POINTS=200000  # if test/validation fix this to 10 000, for train fix this to 250 000
+#####################################################################
 
-cd /cluster/home/abizeul/cl-ica/tools/3dident
+N_BATCHES=10
+mkdir -p ${DIR_DATA}
 
-# python generate_clevr_dataset_latents.py --output-folder "/cluster/home/abizeul/cl-ica/tools/3dident/mydata_positions" --n-points 250000 --non-periodic-rotation-and-color --deterministic --all-positions --multimodal
-# /cluster/home/abizeul/software/blender-2.90.1-linux64/blender -noaudio --background --python generate_clevr_dataset_images.py --use-gpu --output-folder "/cluster/home/abizeul/cl-ica/tools/3dident/mydata_positions" --n-batches 10 --batch-index 0 --material-names "MyMetal" --no_range_change
-# /cluster/home/abizeul/software/blender-2.90.1-linux64/blender -noaudio --background --python generate_clevr_dataset_images.py --use-gpu --output-folder "/cluster/home/abizeul/cl-ica/tools/3dident/mydata_positions" --n-batches 10 --batch-index 1 --material-names "MyMetal" --no_range_change
-# /cluster/home/abizeul/software/blender-2.90.1-linux64/blender -noaudio --background --python generate_clevr_dataset_images.py --use-gpu --output-folder "/cluster/home/abizeul/cl-ica/tools/3dident/mydata_positions" --n-batches 10 --batch-index 2 --material-names "MyMetal" --no_range_change
-# /cluster/home/abizeul/software/blender-2.90.1-linux64/blender -noaudio --background --python generate_clevr_dataset_images.py --use-gpu --output-folder "/cluster/home/abizeul/cl-ica/tools/3dident/mydata_positions" --n-batches 10 --batch-index 3 --material-names "MyMetal" --no_range_change
-# /cluster/home/abizeul/software/blender-2.90.1-linux64/blender -noaudio --background --python generate_clevr_dataset_images.py --use-gpu --output-folder "/cluster/home/abizeul/cl-ica/tools/3dident/mydata_positions" --n-batches 10 --batch-index 4 --material-names "MyMetal" --no_range_change
-# /cluster/home/abizeul/software/blender-2.90.1-linux64/blender -noaudio --background --python generate_clevr_dataset_images.py --use-gpu --output-folder "/cluster/home/abizeul/cl-ica/tools/3dident/mydata_positions" --n-batches 10 --batch-index 5 --material-names "Rubber" --no_range_change
-# /cluster/home/abizeul/software/blender-2.90.1-linux64/blender -noaudio --background --python generate_clevr_dataset_images.py --use-gpu --output-folder "/cluster/home/abizeul/cl-ica/tools/3dident/mydata_positions" --n-batches 10 --batch-index 6 --material-names "Rubber" --no_range_change
-# /cluster/home/abizeul/software/blender-2.90.1-linux64/blender -noaudio --background --python generate_clevr_dataset_images.py --use-gpu --output-folder "/cluster/home/abizeul/cl-ica/tools/3dident/mydata_positions" --n-batches 10 --batch-index 7 --material-names "Rubber" --no_range_change
-# /cluster/home/abizeul/software/blender-2.90.1-linux64/blender -noaudio --background --python generate_clevr_dataset_images.py --use-gpu --output-folder "/cluster/home/abizeul/cl-ica/tools/3dident/mydata_positions" --n-batches 10 --batch-index 8 --material-names "Rubber" --no_range_change
-/cluster/home/abizeul/software/blender-2.90.1-linux64/blender -noaudio --background --python generate_clevr_dataset_images.py --use-gpu --output-folder "/cluster/home/abizeul/cl-ica/tools/3dident/mydata_positions" --n-batches 10 --batch-index 9 --material-names "Rubber" --no_range_change
+source ${CONDA_PATH}
+conda activate alice
 
+cd ${SCRIPT_DIR}
 
-zip -r "/cluster/home/abizeul/cl-ica/tools/3dident/mydata_positions.zip" "/cluster/home/abizeul/cl-ica/tools/3dident/mydata_positions"
-scp "/cluster/home/abizeul/cl-ica/tools/3dident/mydata_positions.zip" "/cluster/work/vogtlab/Group/abizeul/3DIdent/mydata_multimodal_texture_positions_train.zip"
+#python generate_clevr_dataset_latents.py --output-folder ${DIR_DATA} --n-points ${N_POINTS} --non-periodic-rotation-and-color --deterministic --all-positions --multimodal
+i=9
+MATERIAL="MyMetal"
+if [[ $i -ge 5 ]] 
+then
+    MATERIAL="Rubber"
+fi
+echo $MATERIAL
+
+${BLENDER_DIR} -noaudio --background --python generate_clevr_dataset_images.py --use-gpu --output-folder ${DIR_DATA} --n-batches 10 --batch-index ${i} --material-names ${MATERIAL} --no_range_change
 
